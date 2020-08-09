6c50661ddd1f4cf2b0b1f80359386a0f4d063c36
Ronald Paucar <rpaucarc@gmail.com>
2020-08-09 09:50:11 -0500

    subiendo Currency Converter

---
+++ CurrencyConverter/CurrencyConverterLibreria/ConvertibleCurrency.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverterLibreria
+{
+    public abstract class BaseCurrency
+    {
+        public abstract decimal InUS
+        {
+            get;
+        }
+    }
+
+    public class USDCurrency : BaseCurrency
+    {
+        public override decimal InUS
+        {
+            get { return 1; }
+        }
+
+        public override string ToString()
+        {
+            return "USD";
+        }
+    }
+
+    public class PENCurrency : BaseCurrency
+    {
+        public override decimal InUS
+        {
+            get { return 3.53M; }
+        }
+
+        public override string ToString()
+        {
+            return "PEN";
+        }
+    }
+
+    public class EURCurrency : BaseCurrency
+    {
+        public override decimal InUS
+        {
+            get { return 0.85M; }
+        }
+
+        public override string ToString()
+        {
+            return "EUR";
+        }
+    }
+
+    public class ConvertibleCurrency
+    {
+        private decimal amount;
+        private BaseCurrency currency;
+        public ConvertibleCurrency(BaseCurrency type, decimal val)
+        {
+            currency = type;
+            amount = val;
+        }
+
+        public static decimal CurrencyConvert(decimal amount, BaseCurrency fromCur,
+                  BaseCurrency toCur)
+        {
+            decimal converted = 0.0M;
+            ConvertibleCurrency currency = new ConvertibleCurrency(fromCur, amount);
+            converted = currency.ConvertTo(toCur);
+            return converted;
+        }
+
+         public decimal ConvertTo(BaseCurrency type)
+        {
+            decimal converted = ConvertToUS();
+            converted = ConvertFromUS(type, converted);
+            return converted;
+        }
+
+        private decimal ConvertToUS()
+        {
+            decimal converted = 0.0M;
+            converted = amount / currency.InUS;
+            return converted;
+        }
+
+        private decimal ConvertFromUS(BaseCurrency type, decimal USAmount)
+        {
+            decimal converted = 0.0M;
+            converted = USAmount * type.InUS;
+            return converted;
+        }
+    }
+}
---
+++ CurrencyConverter/CurrencyConverterTests/ConvertTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using CurrencyConverterForms;
+using CurrencyConverterLibreria;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CurrencyConverterTests
+{
+    [TestClass]
+    public class ConvertTest
+    {
+        [TestMethod]
+        public void ConversionBreadth()
+        {
+            decimal result;
+            decimal amount;
+            BaseCurrency fromCur;
+            BaseCurrency toCur;
+
+            amount = 100.0M;
+            fromCur = new USDCurrency();
+            toCur = new USDCurrency();
+            result = ConvertibleCurrency.CurrencyConvert(amount, fromCur, toCur);
+            Assert.AreEqual(100.0M, result, "USD to USD should be no change");
+
+            fromCur = new PENCurrency();
+            toCur = new PENCurrency();
+            result = ConvertibleCurrency.CurrencyConvert(amount, fromCur, toCur);
+            Assert.AreEqual(100.0M, result, "PEN to PEN should be no change");
+
+            fromCur = new EURCurrency();
+            toCur = new EURCurrency();
+            result = ConvertibleCurrency.CurrencyConvert(amount, fromCur, toCur);
+            Assert.AreEqual(100.0M, result, "EUR to EUR should be no change");
+
+            decimal expected;
+            fromCur = new USDCurrency();
+            toCur = new EURCurrency();
+            result = ConvertibleCurrency.CurrencyConvert(amount, fromCur, toCur);
+            expected = amount * 0.85M;
+            Assert.AreEqual(expected, result, "USD to EUR is incorrect");
+
+            fromCur = new PENCurrency();
+            toCur = new EURCurrency();
+            result = ConvertibleCurrency.CurrencyConvert(amount, fromCur, toCur);
+            expected = amount / 3.53M * 0.85M;
+            Assert.AreEqual(expected, result,"PEN to EUR is incorrect");
+        }
+
+        [TestMethod]
+        public void ConvertTo()
+        {
+            ConvertibleCurrency currency;
+            decimal result;
+            decimal expected;
+
+            currency = new ConvertibleCurrency(new USDCurrency(), 100.0M);
+            result = currency.ConvertTo(new USDCurrency());
+            Assert.AreEqual(100.0M, result, "USD to USD should be no change");
+
+            currency = new ConvertibleCurrency(new EURCurrency(), 100.0M);
+            result = currency.ConvertTo(new PENCurrency());
+            expected = 100.0M / 0.85M * 3.53M;
+            Assert.AreEqual(expected, result, "EUR to PEN incorrect result");
+        }
+    }
+}
